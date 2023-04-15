788e698051177bc6f7706d931b2a25020a90930a
Yusuf Abacık <yusufabacik@gmail.com>
2023-04-15 19:19:42 +0300

    functionality is done for beta

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,88 @@
+"use client";
 import { useState } from "react";
+import api from "steam-js-api";
 
-export default function Home() {
-  const apiKey = process.env.STEAM_API_KEY;
-  console.log(apiKey);
-  const [nick1, setNick1] = useState("");
-  const [nick2, setNick2] = useState("");
+//const apiKey = process.env.NEXT_PUBLIC_STEAM_API_KEY;
 
-  
-  const getUserGame = async (username) => {
-    // Kullanıcının profil ID'sini almak için Steam API'ye bir istek gönderin
-    fetch(
-      `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${apiKey}&vanityurl=${username}`,
-      { mode: "no-cors" }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.response.success === 1) {
-          const steam_id = data.response.steamid;
-          console.log(steam_id);
+export default function Home({ apiKey }) {
+  const [nick1, setNick1] = useState("Nepcen");
+  const [nick2, setNick2] = useState("SloXen");
+  const [sameGames, setSameGames] = useState([]);
+  const [processStatus, setProcessStatus] = useState(0);
 
-          // Kullanıcının sahip olduğu oyunların listesini almak için Steam API'ye bir istek gönderin
-          fetch(
-            `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steam_id}&format=json`,
-            { mode: "no-cors" }
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              //const games = data.response.games;
-              //games.forEach((game) => console.log(game.name));
-              const res = data.response.games;
-              console.log(res);
-              return res;
-            })
-            .catch((error) => console.error(error));
-        } else {
-          console.error("Kullanıcı adı geçersiz.");
-        }
-      })
-      .catch((error) => console.error(error));
-    return false;
+  api.setKey(apiKey);
+
+  const getUserId = async (username) => {
+    const url = `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${apiKey}&vanityurl=${username}&format=json`
+    try {
+      const result = await fetch(url);
+      const data = await result.json();
+      console.log(data);
+      return data.response.steamid;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   };
 
-  const handleClick = () => {
-    console.log(getUserGame(nick1));
+  const getUserGames = async (userId) => {
+    const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${userId}&format=json`
+    try {
+      const result = await fetch(url);
+      const data = await result.json();
+      console.log(data.response);
+      return data.response;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  const getGameDetail = async (gameId) => {
+    const url = `https://store.steampowered.com/api/appdetails?appids=${gameId}`;
+    try {
+      const result = await fetch(url);
+      const data = await result.json();
+      if (data[gameId].success) {
+        return data[gameId].data;
+      }
+      return { appID: gameId };
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  const handleClick = async () => {
+    setProcessStatus(1);
+    const userGame1 = (await getUserGames(await getUserId(nick1))).games;
+    console.log(userGame1);
+
+    const userGame2 = (await getUserGames(await getUserId(nick2))).games;
+    console.log(userGame2);
+
+    setProcessStatus(2);
+
+    const matchingGames = userGame1.filter((game1) => {
+      return userGame2.some((game2) => game1.appid === game2.appid);
+    });
+    console.log(matchingGames);
+
+    setProcessStatus(3);
+    let sameGamesDetails = [];
+    await Promise.all(
+      matchingGames.map(async (e) => {
+        const gameDetail = await getGameDetail(e.appid);
+        sameGamesDetails.push(gameDetail);
+      })
+    );
+    setProcessStatus(4);
+    console.log(sameGamesDetails);
+    setSameGames(sameGamesDetails);
+  };
+
+  const goster = () => {
+    console.log(sameGames);
   };
 
   return (
@@ -62,7 +101,42 @@ export default function Home() {
           />
         </div>
         <button onClick={handleClick}>Getir</button>
+        <button onClick={goster}>Göster</button>
+      </div>
+
+      <div>
+        {processStatus === 0 && <div>İşlem yapmak için butona basınız.</div>}
+        {processStatus === 1 && <div>İşlem yapılıyor...</div>}
+        {processStatus === 2 && <div>İşlem yapılıyor...</div>}
+        {processStatus === 3 && <div>İşlem yapılıyor...</div>}
+        {processStatus === 4 && (
+          <>
+            <div>İşlem tamamlandı.</div>
+            <div>
+              {sameGames.map((e) => (
+                <div key={`'${e.steam_appid}'`}>
+                  <img src={e.header_image} alt="" />
+                  <div>{e.name}</div>
+                </div>
+              ))}
+            </div>
+            <div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const apiKey = process.env.NEXT_PUBLIC_STEAM_API_KEY;
+
+  console.log(`key: ${apiKey}`);
+
+  return {
+    props: {
+      apiKey: apiKey,
+    },
+  };
 }
